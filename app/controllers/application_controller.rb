3779d089008bbf6907207a84824642058a55b1b4
BuisiX <nwabuisi33@gmail.com>
2015-06-21 20:51:19 -0500

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,3 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
+def hello
+  render text: "<hq>Hello</h1><p>Welcome Home</p>"
+end
+end
